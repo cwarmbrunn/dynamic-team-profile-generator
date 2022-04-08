015cd00f849bf40524d7859a4d759e3a020206cd
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-08 10:06:32 -0400

    added adjustments to employee and manager test

--- __tests___/Manager.test.js
+++ __tests___/Manager.test.js
@@ -9,7 +9,14 @@ describe("Manager Class", () => {
       role: "Manager",
       officeNumber: "1001",
     };
+
+    // describe("Test the name for the manager - should be a string", () => {
+    //   test("Have a valid manager name", () => {
+    //     expect(manager.name).toBe("Artemis");
+    //   });
+    // });
     const manager = new Manager(managerResult);
+    expect(typeof manager).toBe("object");
     expect(manager.name).toBe("Artemis");
     expect(manager.id).toBe("43223");
     expect(manager.email).toBe("artemis@hello.com");
