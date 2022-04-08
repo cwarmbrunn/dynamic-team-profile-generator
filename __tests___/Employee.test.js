8ea58127aa55ce2601a58e6da2e3ad8b05b76d86
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-08 10:55:30 -0400

    added manager test - passes

--- __tests___/Employee.test.js
+++ __tests___/Employee.test.js
@@ -1,21 +1,47 @@
 const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
+// Making employeeResult global
+
+const employeeResult = {
+  name: "Sara",
+  id: "12343",
+  email: "sara@hello.com",
+  role: "Employee",
+};
+
 describe("Employee Class", () => {
   test("Create a employee object", () => {
-    const employeeResult = {
-      name: "Sara",
-      id: "12343",
-      email: "sara@hello.com",
-      role: "Employee",
-    };
     const employee = new Employee(employeeResult);
     expect(typeof employee).toBe("object");
 
-    // Can set name via constructor arguments 
+    // Can set name via constructor arguments
     expect(employee.name).toBe("Sara");
     expect(employee.id).toBe("12343");
     expect(employee.email).toBe("sara@hello.com");
     expect(employee.role).toBe("Employee");
+  });
+
+  // Test the Employee's name
+  describe("Employee Name", () => {
+    test("Test the employee name via constructor arguments", () => {});
+    const employee = new Employee(employeeResult);
+    expect(employee.name).toBe("Sara");
+  });
+
+  // Test the Employee's ID
+  describe("Employee ID", () => {
+    test("Can set id via constructor argument", () => {
+      const employee = new Employee(employeeResult);
+      expect(employee.id).toBe("12343");
+    });
+  });
+
+  // Test the Employee's email
+  describe("Employee Email", () => {
+    test("Can set email via constructor argument", () => {
+      const employee = new Employee(employeeResult);
+      expect(employee.email).toBe("sara@hello.com");
+    });
   });
 });
--- __tests___/Manager.test.js
+++ __tests___/Manager.test.js
@@ -1,15 +1,16 @@
 const Manager = require("../lib/Manager.js");
 
+// Making managerResult global
+const managerResult = {
+  name: "Artemis",
+  id: "43223",
+  email: "artemis@hello.com",
+  role: "Manager",
+  officeNumber: "1001",
+};
+
 describe("Manager Class", () => {
   test("Create a manager object", () => {
-    const managerResult = {
-      name: "Artemis",
-      id: "43223",
-      email: "artemis@hello.com",
-      role: "Manager",
-      officeNumber: "1001",
-    };
-
     const manager = new Manager(managerResult);
     expect(typeof manager).toBe("object");
     expect(manager.name).toBe("Artemis");
@@ -22,14 +23,44 @@ describe("Manager Class", () => {
     // Can get office number via getOffice()
     expect(manager.officeNumber).toBe("1001");
   });
+
+  // Test the manager's name
   describe("Test the name for the manager - should be a string", () => {
     test("Have a valid manager name", () => {
+      const manager = new Manager(managerResult);
       expect(manager.name).toBe("Artemis");
     });
   });
+
+  // Test the manager's employee ID
   describe("Test the manager's employee ID - should be a number", () => {
     test("Have a valid employee ID", () => {
+      const manager = new Manager(managerResult);
       expect(manager.id).toBe("43223");
+    });
+  });
+
+  // Test the manager's email
+  describe("Test the manager's email - should be a string", () => {
+    test("Have a valid email", () => {
+      const manager = new Manager(managerResult);
+      expect(manager.email).toBe("artemis@hello.com");
+    });
+  });
+
+  // Test the manager's role
+  describe("Test the manager's role - should be Manager", () => {
+    test("Have the role be Manager", () => {
+      const manager = new Manager(managerResult);
+      expect(manager.role).toBe("Manager");
+    });
+  });
+
+  // Test the office Number
+  describe("Test the manager's office number - should be a number", () => {
+    test("Have the office number run", () => {
+      const manager = new Manager(managerResult);
+      expect(manager.officeNumber).toBe("1001");
     });
   });
 });
